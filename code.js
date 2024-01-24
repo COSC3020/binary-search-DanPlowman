899ba266b7490ae290adf6bf4f720f9a5f4ed880
DanPlowman <47125582+DanPlowman@users.noreply.github.com>
2024-01-23 17:03:54 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,15 @@
 function binarySearch(list, element) {
+    var min = 0;
+    var max = list.length - 1;
+    while (min <= max)
+    {
+        var guess = Math.floor(max+min/2)
+        if (element === list[guess])
+            {return guess;}
+        else if (element > list[guess])
+            {min = guess+1;}
+        else
+            {max = guess-1;}
+    }
     return -1;
 }
