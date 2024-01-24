cf5d09230b81e20e0686ef3307ca80c4156f3496
DanPlowman <47125582+DanPlowman@users.noreply.github.com>
2024-01-24 15:11:57 -0700

    Update code.js
    this is a bit different of an algorithm I read about on Wikipedia, credited to Hermann Bottenbruch in 1968

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 function binarySearch(list, element) {
-    return -1;
+    var min = 0;
+    var max = list.length - 1;
+    while (min != max)
+    {
+        var guess = Math.ceiling(((max+min)/2));
+        if (element > list[guess])
+            {min = guess+1;}
+        else
+            {max = guess-1;}
+    }
+    if (element === list[guess])
+            {return guess;}
+    else
+    {return -1;}
 }
